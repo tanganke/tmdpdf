6ebb5a7a23b3065a7603ef9b5ba1a5e36577e903
tanganke <tang.anke@foxmail.com>
2023-04-24 00:30:58 +0800

    add verify_str_arg

--- libtmdpdf/utils/__init__.py
+++ libtmdpdf/utils/__init__.py
@@ -2,3 +2,9 @@ R"""
 misc usefull functions and classes.
 """
 from . import color_logging, print_h5, timeit
+from .timeit import *
+from .args import *
+
+
+def first(iterable):
+    return next(iter(iterable))
